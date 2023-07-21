378de6ec34a931e83c4e5c29d446263eeb3ade00
MUHAMMADMEHRAN38 <139791390+MUHAMMADMEHRAN38@users.noreply.github.com>
2023-07-21 12:20:04 +0500

    Add files via upload
    Question no # 2 to 5 solve together of Assignment File

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+//Question no # 2 Of Assignment//
+let personname = ("Hello!Muhammad Mehran would you like to learn some Python today?");
+console.log(personname);
+//Question no # 3 Of Assignment//
+let UPPEERCASE = "ZUHAIB";
+let lowercase = "sultan";
+let Titlecase = "Shair ali";
+console.log(UPPEERCASE);
+console.log(lowercase);
+console.log(Titlecase);
+//Question no # 4 Of Assignment//
+console.log("QUID-E-AZAM once said, “A person who never made a mistake never tried anything new.”");
+//Question no # 5 Of Assignment//
+let famous_person = ("Allama iqbal said, “Nations are born in the hearts of poets, they prosper and die in the hands of politicians.”");
+console.log(famous_person);
